6532046865438e67d4675e61b30a995031c318d5
Andrey Medvedev <a.medvedev@elonsoft.ru>
2022-12-17 17:26:20 +0300

    feat(gRPC): init project

---
+++ gRPC/gRPC/Services/InsuranceService.cs
@@ -0,0 +1,25 @@
+﻿using Grpc.Core;
+using gRPC;
+
+namespace gRPC.Services;
+
+public class InsuranceService : Insurance.InsuranceBase
+{
+    private readonly ILogger<InsuranceService> _logger;
+    public InsuranceService(ILogger<InsuranceService> logger)
+    {
+        _logger = logger;
+    }
+
+    public override Task<InsuranceListReply> InsuranceList(InsuranceListRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new InsuranceListReply
+        {
+            Id = "id",
+            Title = "title",
+            Category = "category",
+            InsuranceAmount = 10000
+        });
+    }
+}
+
