fc0191e1eb78fd177eb1f78b8ddd6393e5393afa
Andrey Medvedev <a.medvedev@elonsoft.ru>
2022-12-25 01:56:09 +0300

    feat(gRPC): add auth service

--- API/API/Controllers/AuthController.cs
+++ API/API/Controllers/AuthController.cs
@@ -9,6 +9,7 @@ using System.Text;
 using API.Models.Auth;
 using API.Models.User;
 using API.DBContext;
+using System.Security.Claims;
 
 [ApiController]
 [Route("api/[controller]")]
@@ -49,6 +50,52 @@ public class AuthController : ControllerBase
         }
         string res = JsonSerializer.Serialize<User>(user);
         return res;
+    }
+
+    [HttpPost("getToken")]
+    public string GetToken([FromBody] Credentials credentials)
+    {
+        if (credentials.email.Length == 0 || credentials.password.Length == 0)
+        {
+            new HttpResponseException(400);
+            return "error";
+        }
+
+        string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+            password: credentials.password,
+            salt: Encoding.ASCII.GetBytes(Configuration["salt"]),
+            prf: KeyDerivationPrf.HMACSHA256,
+            iterationCount: 100000,
+            numBytesRequested: 256 / 8)
+        );
+
+        var user = db.user.First(p => p.email == credentials.email);
+
+        if (user.passwordhash != hashed)
+        {
+            new HttpResponseException(401);
+            return "error";
+        }
+        return GenerateToken(credentials);
+    }
+
+    private string GenerateToken(Credentials credentials)
+    {
+        var credentials = new SigningCredentials(Configuration["salt"], SecurityAlgorithms.HmacSha256);
+        var claims = new[]
+        {
+                new Claim(ClaimTypes.NameIdentifier,user.Username),
+                new Claim(ClaimTypes.Role,user.Role)
+            };
+        var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+            _config["Jwt:Audience"],
+            claims,
+            expires: DateTime.Now.AddMinutes(15),
+            signingCredentials: credentials);
+
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+
     }
 
 }
--- gRPC/gRPC/Services/InsuranceService.cs
+++ gRPC/gRPC/Services/InsuranceService.cs
@@ -2,6 +2,7 @@
 using gRPC.DBContext;
 using gRPC.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Text;
 
 namespace gRPC.Services;
 
@@ -17,6 +18,13 @@ public class InsuranceService : Insurance.InsuranceBase
 
     public override Task<InsuranceListReply> InsuranceList(InsuranceListRequest request, ServerCallContext context)
     {
+        var decryptedToken = DecryptJWTToket(request.Token);
+
+        if (decryptedToken.Length == 0)
+        {
+            throw new Exception("unothorizued");
+        }
+
         InsuranceListReply reply = new InsuranceListReply();
 
         var insurances = db.insurance.FromSql($"select id, title, category, insurance_amount from insurance;").ToList();
@@ -35,6 +43,44 @@ public class InsuranceService : Insurance.InsuranceBase
         }
 
         return Task.FromResult(reply);
+    }
+
+    public override Task<InsuranceItem> AddInsurance(AddInsuranceRequest request, ServerCallContext context)
+    {
+        var decryptedToken = DecryptJWTToket(request.Token);
+
+        if (decryptedToken.Length == 0)
+        {
+            throw new Exception("unothorizued");
+        }
+
+        var newInsurance = new gRPC.Models.Insurance(request.Title, request.Category, request.InsuranceAmount);
+
+        var insuranceReply = db.insurance.Add(newInsurance);
+
+        var temp = new InsuranceItem
+        {
+            Id = insuranceReply.id.ToString(),
+            Title = insuranceReply.title,
+            Category = insuranceReply.category,
+            InsuranceAmount = insuranceReply.insurance_amount
+        };
+
+        return temp;
+    }
+
+    private string DecryptJWTToket(string token)
+    {
+        var handler = new JwtSecurityTokenHandler();
+        var validations = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Configuration["salt"]),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+        var claims = handler.ValidateToken(token, validations, out var tokenSecure);
+        return claims.Identity.Name;
     }
 }
 
